560e79e71331ff35d63bf068cca0087562e3cf44
Pratham Mahajan <mahajanpratham@gmail.com>
2024-07-31 12:13:21 +0530

    Hero page updated

--- frontend/src/components/ConnectWalletButton.tsx
+++ frontend/src/components/ConnectWalletButton.tsx
@@ -13,16 +13,6 @@ export const ConnectWalletButton = () => {
   ];
 
   return (
-    // <Button
-    //   className="w-72 rounded-full bg-gray-500 text-lg text-black font-normal normal-case py-3"
-    //   size="lg"
-    //   placeholder={undefined}
-    //   onPointerEnterCapture={undefined}
-    //   onPointerLeaveCapture={undefined}
-    // >
-    //   Connect Wallet
-    // </Button>
-
     <ConnectButton
       client={client}
       chains={[arbitrumSepolia, sepolia]}
--- frontend/src/pages/Hero.tsx
+++ frontend/src/pages/Hero.tsx
@@ -1,14 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { ConnectWalletButton } from "../components/ConnectWalletButton";
-import { useActiveWalletConnectionStatus } from "thirdweb/react";
+import {
+  useActiveAccount,
+  useActiveWalletConnectionStatus,
+} from "thirdweb/react";
 import { useAppDispatch } from "../app/hooks";
 import { connectWallet } from "../app/features/connectWalletSlice";
-import { Button, Typography } from "@material-tailwind/react";
+import { Button, Spinner, Typography } from "@material-tailwind/react";
 import { motion } from "framer-motion";
+import { useNavigate } from "react-router-dom";
 
 const Hero = () => {
   const dispatch = useAppDispatch();
   const connectionStatus = useActiveWalletConnectionStatus();
+  const account = useActiveAccount();
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (connectionStatus === "connected") {
@@ -18,6 +25,23 @@ const Hero = () => {
       console.log("not connected");
     }
   }, [connectionStatus]);
+
+  const launchApp = () => {
+    if (!account) {
+      alert("Please connect your wallet to launch the app");
+      return;
+    }
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      var onboarded = false;
+      if (onboarded) {
+        navigate("/public");
+      } else {
+        navigate("/onboarding/public");
+      }
+    }, 2000);
+  };
 
   return (
     <motion.div
@@ -46,13 +70,26 @@ const Hero = () => {
             color="white"
             className="w-72 !bg-app-gradient rounded-full text-black font-normal"
             size="lg"
+            disabled={loading}
+            onClick={launchApp}
             placeholder={undefined}
             onPointerEnterCapture={undefined}
             onPointerLeaveCapture={undefined}
           >
-            <div className="flex items-center justify-center gap-2">
-              Launch app
-            </div>
+            {loading ? (
+              <div className="flex items-center justify-center gap-2">
+                <Spinner
+                  className="w-4 h-4"
+                  onPointerEnterCapture={undefined}
+                  onPointerLeaveCapture={undefined}
+                />
+                Launching app
+              </div>
+            ) : (
+              <div className="flex items-center justify-center gap-2">
+                Launch app
+              </div>
+            )}
           </Button>
           <ConnectWalletButton />
         </div>
@@ -72,34 +109,45 @@ const Hero = () => {
         >
           <div className="h-screen flex items-center justify-between">
             <motion.div
-              className="flex-1 flex flex-col justify-center items-center gap-4"
+              className="flex-1 flex justify-center items-center"
               initial={{ x: -100, opacity: 0 }}
               whileInView={{ x: 0, opacity: 1 }}
               transition={{ duration: 0.8, delay: 0.2 }}
               viewport={{ amount: 0.3 }}
             >
-              <Typography
-                variant="h2"
-                color="black"
-                placeholder={undefined}
-                onPointerEnterCapture={undefined}
-                onPointerLeaveCapture={undefined}
-              >
-                Public Asset Management
-              </Typography>
-              <Typography
-                variant="paragraph"
-                color="black"
-                className="w-96"
-                placeholder={undefined}
-                onPointerEnterCapture={undefined}
-                onPointerLeaveCapture={undefined}
-              >
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled
-              </Typography>
+              <div className="flex flex-col justify-center items-start gap-4">
+                <Typography
+                  variant="h2"
+                  color="black"
+                  placeholder={undefined}
+                  onPointerEnterCapture={undefined}
+                  onPointerLeaveCapture={undefined}
+                >
+                  Public Asset Management
+                </Typography>
+                <Typography
+                  variant="paragraph"
+                  color="black"
+                  className="w-[600px]"
+                  placeholder={undefined}
+                  onPointerEnterCapture={undefined}
+                  onPointerLeaveCapture={undefined}
+                >
+                  Securely manage and track your public assets with our advanced
+                  blockchain solution. Enjoy transparent transactions, real-time
+                  updates, and seamless integration across multiple chains. Our
+                  system seamlessly integrates with multiple blockchain
+                  networks, allowing you to manage assets across different
+                  chains from a single interface. Real-time updates ensure you
+                  always have the most up-to-date information about your
+                  holdings. Transparency is key, with clear audit trails and
+                  increased trust in transactions. Our intuitive design makes it
+                  easy for both novice and experienced users to navigate and
+                  manage their public assets effectively. Robust security
+                  protocols are in place to protect your public assets, even
+                  though they are visible on the blockchain.
+                </Typography>
+              </div>
             </motion.div>
             <motion.img
               src="/app-public-model.png"
@@ -130,34 +178,43 @@ const Hero = () => {
               viewport={{ amount: 0.3 }}
             />
             <motion.div
-              className="flex-1 flex flex-col justify-center items-center gap-4"
+              className="flex-1 flex justify-center items-center"
               initial={{ x: 100, opacity: 0 }}
               whileInView={{ x: 0, opacity: 1 }}
               transition={{ duration: 0.8, delay: 0.4 }}
               viewport={{ amount: 0.3 }}
             >
-              <Typography
-                variant="h2"
-                color="black"
-                placeholder={undefined}
-                onPointerEnterCapture={undefined}
-                onPointerLeaveCapture={undefined}
-              >
-                Private Asset Management
-              </Typography>
-              <Typography
-                variant="paragraph"
-                color="black"
-                className="w-96"
-                placeholder={undefined}
-                onPointerEnterCapture={undefined}
-                onPointerLeaveCapture={undefined}
-              >
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled
-              </Typography>
+              <div className="flex flex-col justify-center items-start gap-4">
+                <Typography
+                  variant="h2"
+                  color="black"
+                  placeholder={undefined}
+                  onPointerEnterCapture={undefined}
+                  onPointerLeaveCapture={undefined}
+                >
+                  Private Asset Management
+                </Typography>
+                <Typography
+                  variant="paragraph"
+                  color="black"
+                  className="w-[600px]"
+                  placeholder={undefined}
+                  onPointerEnterCapture={undefined}
+                  onPointerLeaveCapture={undefined}
+                >
+                  Protect your financial privacy with our cutting-edge private
+                  asset management system. Utilize stealth addresses and
+                  encrypted transactions to keep your assets confidential and
+                  secure. Our private asset management feature maintains a
+                  balance between user privacy and regulatory compliance,
+                  addressing potential concerns about illicit activities. You
+                  have full control over your financial privacy, allowing you to
+                  choose when and how to disclose your asset information.
+                  Seamlessly switch between managing your public and private
+                  assets, providing a comprehensive financial management
+                  experience.
+                </Typography>
+              </div>
             </motion.div>
           </div>
         </motion.section>
@@ -170,34 +227,46 @@ const Hero = () => {
         >
           <div className="h-screen flex items-center justify-between">
             <motion.div
-              className="flex-1 flex flex-col justify-center items-center gap-4"
+              className="flex-1 flex justify-center items-center"
               initial={{ y: 100, opacity: 0 }}
               whileInView={{ y: 0, opacity: 1 }}
               transition={{ duration: 0.8, delay: 0.2 }}
               viewport={{ amount: 0.3 }}
             >
-              <Typography
-                variant="h2"
-                color="black"
-                placeholder={undefined}
-                onPointerEnterCapture={undefined}
-                onPointerLeaveCapture={undefined}
-              >
-                Private Transactions
-              </Typography>
-              <Typography
-                variant="paragraph"
-                color="black"
-                className="w-96"
-                placeholder={undefined}
-                onPointerEnterCapture={undefined}
-                onPointerLeaveCapture={undefined}
-              >
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled
-              </Typography>
+              <div className="flex flex-col justify-center items-start gap-4">
+                <Typography
+                  variant="h2"
+                  color="black"
+                  placeholder={undefined}
+                  onPointerEnterCapture={undefined}
+                  onPointerLeaveCapture={undefined}
+                >
+                  Private Transactions
+                </Typography>
+                <Typography
+                  variant="paragraph"
+                  color="black"
+                  className="w-[600px]"
+                  placeholder={undefined}
+                  onPointerEnterCapture={undefined}
+                  onPointerLeaveCapture={undefined}
+                >
+                  Experience truly private transactions with our
+                  state-of-the-art technology. Send and receive assets
+                  anonymously, maintaining complete control over your financial
+                  information and activity. Our advanced cryptographic
+                  techniques, such as zero-knowledge proofs or ring signatures,
+                  ensure transaction privacy. The system maintains user
+                  anonymity throughout the transaction process, from initiation
+                  to completion. Conduct private transactions across different
+                  blockchain networks, expanding the scope of financial privacy.
+                  You can selectively reveal transaction details for auditing
+                  purposes when necessary, maintaining a balance between privacy
+                  and accountability. The process of conducting private
+                  transactions is seamless and user-friendly, making advanced
+                  privacy features accessible to all users.
+                </Typography>
+              </div>
             </motion.div>
             <motion.img
               src="/app-transaction-model.png"
